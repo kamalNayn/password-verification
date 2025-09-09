d93039666d5035fc055520d73cc2b75df6cbdca1
Kamal <aryaclwn2025@gmail.com>
2025-09-09 09:29:06 +0000

    Feature1: Password is OK if atleast three condition is met

--- src/main/java/org/kamal/util/PasswordValidator.java
+++ src/main/java/org/kamal/util/PasswordValidator.java
@@ -8,41 +8,61 @@ public class PasswordValidator {
         Validate Password
      */
     public static boolean isValidPassword(String password){
-        //check if password is not null
-        if (password != null) {
-            // check password length should be larger than 8
-            if(password.length()<8){
-                throw new IncorrectPasswordException("Password should be larger than 8 chars");
-            }
-            int hasOneUpperCase = 0;
-            int hasOneLowerCase = 0;
-            int hasOneNumber = 0;
+        //conditionMet counter
+        int conditionMet = 0;
 
-            for(char c: password.toCharArray()){
-                if (Character.isUpperCase(c)) {
-                    hasOneUpperCase++;
-                } else if (Character.isLowerCase(c)) {
-                    hasOneLowerCase++;
-                } else if (Character.isDigit(c)) {
-                    hasOneNumber++;
+        try{
+            //check if password is not null
+            if (password != null) {
+                conditionMet++;
+
+                // check password length should be larger than 8
+                if(password.length()<8){
+                    throw new IncorrectPasswordException("Password should be larger than 8 chars");
+                }else {
+                    conditionMet++;
                 }
-            }
-            //check if password has one upper case
-            if(hasOneUpperCase==0){
-                throw new IncorrectPasswordException("Password should have one uppercase letter at least");
-            }
-            //check if password has one lower case
-            else if(hasOneLowerCase==0){
-                throw new IncorrectPasswordException("Password should have one lowercase letter at least");
-            }
-            //check if password has one number
-            else if(hasOneNumber==0){
-                throw new IncorrectPasswordException("Password should have one number at least");
-            }
+                int hasOneUpperCase = 0;
+                int hasOneLowerCase = 0;
+                int hasOneNumber = 0;
 
-        }else{
-            throw new IncorrectPasswordException("Password should not be null");
+                for(char c: password.toCharArray()){
+                    if (Character.isUpperCase(c)) {
+                        hasOneUpperCase++;
+                    } else if (Character.isLowerCase(c)) {
+                        hasOneLowerCase++;
+                    } else if (Character.isDigit(c)) {
+                        hasOneNumber++;
+                    }
+                }
+                //check if password has one upper case
+                if(hasOneUpperCase==0){
+                    throw new IncorrectPasswordException("Password should have one uppercase letter at least");
+                }else{
+                    conditionMet++;
+                }
+                //check if password has one lower case
+                if(hasOneLowerCase==0){
+                    throw new IncorrectPasswordException("Password should have one lowercase letter at least");
+                }else {
+                    conditionMet++;
+                }
+                //check if password has one number
+                if(hasOneNumber==0){
+                    throw new IncorrectPasswordException("Password should have one number at least");
+                }else {
+                    conditionMet++;
+                }
+                return conditionMet >= 3;
+            }else{
+                throw new IncorrectPasswordException("Password should not be null");
+            }
+        }catch (IncorrectPasswordException e){
+            if(conditionMet>=3){
+                return true;
+            }else{
+                throw e;
+            }
         }
-        return true;
     }
 }
